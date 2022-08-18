7484b64ab9c1c4b2cb69d040a8b70904bd5c4e60
duro06 <andi.meka.025@gmail.com>
2022-08-18 23:03:20 +0700

    Beban: controller, resource, api

---
+++ routes/v1/beban.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Api\v1\BebanController;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'middleware' => 'auth:api',
+    // 'middleware' => 'jwt.verify',
+    'prefix' => 'beban'
+], function () {
+    Route::get('/index', [BebanController::class, 'index']);
+    Route::post('/store', [BebanController::class, 'store']);
+    Route::post('/destroy', [BebanController::class, 'destroy']);
+});
