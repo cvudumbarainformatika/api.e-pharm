e2ce283348d73629efdaf8e16b16996f64f29684
duro06 <andi.meka.025@gmail.com>
2023-02-15 05:10:16 +0700

    kasir

--- app/Http/Controllers/Api/v1/KasirController.php
+++ app/Http/Controllers/Api/v1/KasirController.php
@@ -15,10 +15,14 @@ class KasirController extends Controller
         $user = auth()->user();
         $kasir_id = $user->role === 'kasir' ? $user->id : request('kasir_id');
         $penjualan = $this->hitung($kasir_id, 'PENJUALAN');
+        $penjualanNon = $this->hitungNonTunai($kasir_id, 'PENJUALAN');
         $retur_penjualan = $this->hitung($kasir_id, 'RETUR PENJUALAN');
+        $retur_penjualanNon = $this->hitungNonTunai($kasir_id, 'RETUR PENJUALAN');
         $pengeluaran = $this->beban($kasir_id, 'PENGELUARAN');
         $data['penjualan'] = $penjualan;
+        $data['penjualanNon'] = $penjualanNon;
         $data['retur_penjualan'] = $retur_penjualan;
+        $data['retur_penjualanNon'] = $retur_penjualanNon;
         $data['pengeluaran'] = $pengeluaran;
         return new JsonResponse($data);
     }
@@ -30,6 +34,21 @@ class KasirController extends Controller
                     ->whereDate('tanggal', date('Y-m-d'))
                     ->where('nama', $nama)
                     ->where('status', '>=', 2)
+                    ->where('jenis', 'tunai')
+                    ->where('kasir_id',  $kasir);
+            })->get();
+
+        return $data;
+    }
+    public function hitungNonTunai($kasir, $nama)
+    {
+        $data = DetailTransaction::selectRaw('sum(sub_total) as uang')
+            ->whereHas('transaction', function ($f) use ($kasir, $nama) {
+                $f->where('status', '>=', 2)
+                    ->whereDate('tanggal', date('Y-m-d'))
+                    ->where('nama', $nama)
+                    ->where('status', '>=', 2)
+                    ->where('jenis', 'non-tunai')
                     ->where('kasir_id',  $kasir);
             })->get();
 
