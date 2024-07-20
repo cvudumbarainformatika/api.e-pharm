fefb9ac8a7b9ba1cda6de460ef719b6eacf4228b
duro06 <andi.meka.025@gmail.com>
2024-07-20 22:10:44 +0700

    prod

--- database/migrations/2022_08_15_163206_create_products_table.php
+++ database/migrations/2022_08_15_163206_create_products_table.php
@@ -27,6 +27,7 @@ class CreateProductsTable extends Migration
             $table->double('harga_jual_umum', 20, 2)->default(0);
             $table->double('harga_jual_resep', 20, 2)->default(0);
             $table->double('harga_jual_cust', 20, 2)->default(0);
+            $table->double('harga_jual_prem', 20, 2)->default(0);
             $table->double('stok_awal')->default(0);
             $table->double('limit_stok')->default(2);
             $table->unsignedBigInteger('rak_id')->nullable();
