23042a24fa964ce46912422dd6cd3e13404614da
duro06 <andi.meka.025@gmail.com>
2024-08-31 06:55:53 +0700

    persiaoan tabel pemesanan

---
+++ database/migrations/2024_08_31_064719_create_pemesanans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePemesanansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pemesanans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nopemesanan', 50)->default('');
+            $table->date('tgl_pemesanan')->default(null);
+            $table->string('kode_supplier', 50)->default('');
+            $table->string('flag', 2)->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pemesanans');
+    }
+}
---
+++ database/migrations/2024_08_31_064812_create_detail_pemesanans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailPemesanansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_pemesanans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nopemesanan', 50)->default('');
+            $table->string('kode_produk', 50)->default('');
+            $table->double('qty', 24, 2)->default(0);
+            $table->double('harga', 24, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_pemesanans');
+    }
+}
