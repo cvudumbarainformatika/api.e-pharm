f2187c007e34f01a1de2c15392efe439476dfc64
duro06 <andi.meka.025@gmail.com>
2025-01-19 11:15:46 +0700

    rev : new fitur transaksi kasir

--- app/Http/Controllers/Api/v1/KasirController.php
+++ app/Http/Controllers/Api/v1/KasirController.php
@@ -7,6 +7,7 @@ use App\Models\BebanTransaction;
 use App\Models\DetailTransaction;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class KasirController extends Controller
 {
@@ -14,17 +15,50 @@ class KasirController extends Controller
     {
         $user = auth()->user();
         $kasir_id = $user->role === 'kasir' ? $user->id : request('kasir_id');
+        $super = in_array($user->role, ['root', 'owner']);
         $penjualan = $this->hitung($kasir_id, 'PENJUALAN');
-        $penjualanNon = $this->hitungNonTunai($kasir_id, 'PENJUALAN');
         $retur_penjualan = $this->hitung($kasir_id, 'RETUR PENJUALAN');
-        $retur_penjualanNon = $this->hitungNonTunai($kasir_id, 'RETUR PENJUALAN');
-        $pengeluaran = $this->beban($kasir_id, 'PENGELUARAN');
+        $newpenjualan = $this->newHitung($super, $kasir_id, 'PENJUALAN');
+        $newretur_penjualan = $this->newHitung($super, $kasir_id, 'RETUR PENJUALAN');
         $data['penjualan'] = $penjualan;
-        $data['penjualanNon'] = $penjualanNon;
+        $data['newpenjualan'] = $newpenjualan;
         $data['retur_penjualan'] = $retur_penjualan;
-        $data['retur_penjualanNon'] = $retur_penjualanNon;
-        $data['pengeluaran'] = $pengeluaran;
+        $data['newretur_penjualan'] = $newretur_penjualan;
+        $data['super'] = $super;
+        // $penjualanNon = $this->hitungNonTunai($kasir_id, 'PENJUALAN');
+        // $retur_penjualanNon = $this->hitungNonTunai($kasir_id, 'RETUR PENJUALAN');
+        // $pengeluaran = $this->beban($kasir_id, 'PENGELUARAN');
+        // $data['penjualanNon'] = $penjualanNon;
+        // $data['retur_penjualanNon'] = $retur_penjualanNon;
+        // $data['pengeluaran'] = $pengeluaran;
         return new JsonResponse($data);
+    }
+    public function newHitung($super, $kasir, $nama)
+    {
+        $data = DetailTransaction::select(
+            DB::raw('sum(detail_transactions.sub_total) as uang'),
+            'transactions.kasir_id',
+            'detail_transactions.transaction_id'
+
+        )
+            ->leftJoin('transactions', 'transactions.id', '=', 'detail_transactions.transaction_id')
+            ->where('transactions.status', '>=', 2)
+            ->where('transactions.tanggal', 'like', '%' . date('Y-m-d') . '%')
+            ->where('transactions.nama', $nama)
+            ->where('transactions.status', '>=', 2)
+            ->where('transactions.jenis', 'tunai')
+            ->when(
+                $super,
+                function ($q) {
+                    $q->groupBy('transactions.kasir_id');
+                },
+                function ($q) use ($kasir) {
+                    $q->where('transactions.kasir_id',  $kasir);
+                }
+            )
+            ->with('transaction:id,kasir_id', 'transaction.kasir:id,name,role')
+            ->get();
+        return $data;
     }
     public function hitung($kasir, $nama)
     {
@@ -36,7 +70,8 @@ class KasirController extends Controller
                     ->where('status', '>=', 2)
                     ->where('jenis', 'tunai')
                     ->where('kasir_id',  $kasir);
-            })->get();
+            })
+            ->get();
 
         return $data;
     }
@@ -50,7 +85,8 @@ class KasirController extends Controller
                     ->where('status', '>=', 2)
                     ->where('jenis', 'non-tunai')
                     ->where('kasir_id',  $kasir);
-            })->get();
+            })
+            ->get();
 
         return $data;
     }
@@ -62,7 +98,8 @@ class KasirController extends Controller
                     ->whereDate('tanggal', date('Y-m-d'))
                     ->where('nama', $nama)
                     ->where('kasir_id',  $kasir);
-            })->get();
+            })
+            ->get();
 
         return $data;
     }
